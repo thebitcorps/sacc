2c4f00e73a6cf72744968c5ccfbe69ace610b292
Diego Llamas <diegollams@gmail.com>
2015-05-09 07:38:51 -0500

    refactor gender

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  MALE = { :value => true, :display_name => "Male" }
+  FEMALE = { :value => false, :display_name => "Female" }
+
+  def self.get_gender_dropdown
+    {MALE[:display_name] => MALE[:value], FEMALE[:display_name]=> MALE[:value]}
+  end
+
+
+
 end
