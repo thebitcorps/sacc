23a1235916da0c4f3b2a4028db962d5edd50fc23
Diego Llamas <diegollams@gmail.com>
2015-05-11 16:14:13 -0500

    add directions to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,9 +3,10 @@ class Client < ActiveRecord::Base
   belongs_to :salesman, :class_name => "User", :foreign_key => "current_salesman_id"
   has_many :visits, :class_name => "Visit"
   has_many :phones, dependent: :destroy
+  has_many :addresses, dependent: :destroy
 
   accepts_nested_attributes_for :phones,reject_if: :all_blank, allow_destroy: true
-
+  accepts_nested_attributes_for :addresses,reject_if: :all_blank, allow_destroy: true
 
   def fullname
     self.name + ' ' + self.paternal_lastname + ' ' + self.maternal_lastname
