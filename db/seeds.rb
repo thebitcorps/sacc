5fff72375364a9d43387577111dfa9a18a7c97df
Roberto Ruiz-Velasco Martinez <roberto@artificemultimedia.com>
2015-05-12 00:15:02 -0500

    small fixes, still cant make materials menu work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: 'Klaus', paternal_lastname: 'Ruber', maternal_lastname: 'Toe',
-            password: '12345678', email: 'klaus@axa.com', cellphone: '(449) 123 1234', gender: true)
+User.create(name: 'Developer', paternal_lastname: 'Bit', maternal_lastname: 'Thor',
+            password: '12345678', email: 'dev@thebitcorps.com', cellphone: '(449) 123 1234', gender: true)
+Client.create(name: 'Juan', paternal_lastname: 'De Las', maternal_lastname: 'Cuerdas', current_salesman_id: 1, gender: true)
