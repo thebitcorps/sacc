5ce39f40b8e4c1bc75b8a5317f1a1b17dd1d1151
Roberto Ruiz-Velasco Martinez <roberto@artificemultimedia.com>
2015-05-17 00:18:48 -0500

    slimmed appointments, modified seeds, again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 3.times do |i|
   User.create(name: "Developer", paternal_lastname: "Bit #{i}", maternal_lastname: "Thor #{i}",
-              password: '12345678', email: 'dev@thebitcorps.com', cellphone: '(449) 123 1234', gender: true)
+              password: '12345678', email: "dev#{i}@thebitcorps.com", cellphone: '(449) 123 1234', gender: true)
 end
 
 5.times do |i|
