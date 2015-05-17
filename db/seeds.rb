7951713311d5ff6d18e2e2dddb6d0ce38dfa2712
Roberto Ruiz-Velasco Martinez <roberto@artificemultimedia.com>
2015-05-16 23:42:25 -0500

    working on user dashboard, small fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-User.create(name: 'Developer', paternal_lastname: 'Bit', maternal_lastname: 'Thor',
-            password: '12345678', email: 'dev@thebitcorps.com', cellphone: '(449) 123 1234', gender: true)
+3.times do |i|
+  User.create(name: "Developer", paternal_lastname: "Bit #{i}", maternal_lastname: "Thor #{i}",
+              password: '12345678', email: 'dev@thebitcorps.com', cellphone: '(449) 123 1234', gender: true)
+end
 
 5.times do |i|
-  Client.create(name: "Juan#{i}", paternal_lastname: 'De Las', maternal_lastname: 'Cuerdas', current_salesman_id: 1, gender: true)
-  Client.create(name: "Diego#{i}", paternal_lastname: 'De Las', maternal_lastname: 'Cuerdas', current_salesman_id: 1, gender: true)
-  Client.create(name: "Gaviota#{i}", paternal_lastname: 'Pena', maternal_lastname: 'Miento', current_salesman_id: 1, gender: false)
+  Client.create(name: "Juan #{i}", paternal_lastname: 'De Las', maternal_lastname: 'Cuerdas', current_salesman_id: [1,2,3].sample, gender: true)
+  Client.create(name: "Diego #{i}", paternal_lastname: 'De Las', maternal_lastname: 'Cuerdas', current_salesman_id: [1,2,3].sample, gender: true)
+  Client.create(name: "Gaviota #{i}", paternal_lastname: 'Pena', maternal_lastname: 'Miento', current_salesman_id: [1,2,3].sample, gender: false)
 end
