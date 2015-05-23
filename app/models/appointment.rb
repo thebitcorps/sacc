8093f9d7748a83b746c2cc7a69b27657edba198c
Roberto Ruiz-Velasco Martinez <roberto@artificemultimedia.com>
2015-05-22 23:57:06 -0500

    reverse engineering template, fixed missing assets, looking good :)

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,17 @@
 class Appointment < ActiveRecord::Base
-  STATUS = %w(cancelada reprogramada proxima exitosa)
+  STATUS = %w(Next Successful Rescheduled Cancelled)
 
   belongs_to :client
   belongs_to :user #, counter_cache: true
   validates :date,:time, presence: true
+
+  scope :future_date, ->(date) { where("date >= ?", date)}
+  scope :past_date, ->(date) { where("date < ?", date)}
+
+  # Just an idea Jams
+  def self.upcoming_from(user_id, date)
+    where(user_id: user_id).future_date(date).order(:date)
+  end
 
   def self.type_list
     %w[all today upcoming previous]
@@ -15,10 +23,6 @@ class Appointment < ActiveRecord::Base
 
   def self.today_from(user_id)
     where(date: Date.today,user_id: user_id).order('date')
-  end
-
-  def self.upcoming_from(user_id)
-    where( "date > ? and user_id = ?" ,Date.today, user_id ).order('date')
   end
 
   def self.previous_from(user_id)
--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -1,12 +1,16 @@
 class MessageWorker
+  # with great power comes great responsability. please handle with care.
+  TWILIO_NUMBER = '528153509504'
+  TWILIO_ACCOUNT_SID = 'AC8d52061961139162598aecad2b6868f0'
+  TWILIO_AUTH_TOKEN = '27ca152b4fe82d0107cc1b1cf488098c'
 	include Sidekiq::Worker
 
 	def perform(message_id)
 		message = Message.find(message_id)
 		message.sent = true
 		message.save
-		#twilio_number = ENV['TWILIO_NUMBER']
-    	#client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+		#twilio_number = TWILIO_NUMBER
+    	#client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
     	#client.account.messages.create(from: @twilio_number, to: message.phone_number, body: message.body)
 	end
 end
