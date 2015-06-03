1c3aa3b2d8ebc6a65a20bc8e34bce78ec1e3fdea
Diego Llamas <diegollams@gmail.com>
2015-06-03 13:41:57 -0500

    UI fixes

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -4,6 +4,15 @@ module AppointmentsHelper
     date.strftime("%A, %b %d") # final format for spanish would be "nombre_del_dia, dia de mes"
   end
 
+  def no_appoitments_messages(type_list)
+    if type_list
+      hash = {'today' => 'No appointments today', 'upcoming' => 'No future appointments', 'all' => 'No appointments yet', 'previous' => 'There are mo previous appointments'}
+      hash[type_list]
+    else
+      'No appoitments today'
+    end
+  end
+
   def today_lambda
     -> {Date.today}
   end
