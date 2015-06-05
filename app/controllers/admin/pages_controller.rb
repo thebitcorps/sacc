c98821ba9d34e652948407e3d039d529e031e3ed
Omar de Anda <omarandstuff@gmail.com>
2015-06-05 14:08:21 -0500

    Admin name space rout and small admin info

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,7 +2,10 @@ class Admin::PagesController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-
+    @salesmen = User.salesmen
+    @clients = Client.all
+    @profiled_clients = Client.where(profiled: true)
+    @potential_clients = Client.where(potential: true)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ admin = User.create(name: "Admin", paternal_lastname: "Bit", maternal_lastname: 
               password: 'admin123', email: "admin@thebitcorps.com", cellphone: '(449) 123 1234', gender: true)
 admin.admin!
 3.times do |i|
-  User.create(name: "Developer", paternal_lastname: "Bit #{i}", maternal_lastname: "Thor #{i}",
+  user = User.create(name: "Developer", paternal_lastname: "Bit #{i}", maternal_lastname: "Thor #{i}",
               password: '12345678', email: "dev#{i}@thebitcorps.com", cellphone: '(449) 123 1234', gender: true)
+  user.salesman!
 end
